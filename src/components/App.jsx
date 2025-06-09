76a78a971309c233e570f137c5feb3b691b02acb
raquelpz <rpz.ra.chel@gmail.com>
2025-06-09 20:25:13 +0200

    Filters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,85 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/App.scss';
-import data from '../data/magic.json';
+// import data from '../data/magic.json';
 
 function App() {
 
   //VAR ESTADO
-  const [magic, setMagic] = useState(data);
-  console.log(data);
+  const [magic, setMagic] = useState([]);
+  const [filters, setFilters] = useState({
+    name:'',
+    house:'',
+    ancestry:''
+  });
 
-  //EVENTOS
+  //DATA: USE-EFFECTS
+  useEffect( () => { 
+    fetch('https://hp-api.onrender.com/api/characters')
+      .then(response => response.json())
+      .then(data => {
+        setMagic(data);
+      });
+  }, []);
+
+  //EVENTS
+  const handleFilterName = (ev) => {
+    setFilters({
+      ...filters,
+      name: ev.target.value
+    });
+  };
+  const handleFilterHouse = (ev) => {
+    setFilters({
+      ...filters,
+      house: ev.target.value
+    });
+  };
+  const handleFilterBlood = (ev) => {
+    setFilters({
+      ...filters,
+      ancestry: ev.target.value
+    });
+  };
+
+  const filteredMagic = magic
+    .filter((eachMagic) =>
+      eachMagic.name.toLocaleLowerCase()
+        .includes(filters.name.toLocaleLowerCase())
+  )
+    .filter((eachMagic) =>
+      eachMagic.house
+        .includes(filters.house)
+  )
+    .filter((eachMagic) =>
+      eachMagic.ancestry
+        .includes(filters.ancestry)
+  );
   
-
   //FUNCTIONS
+
 
   return (
     <div className='page'>
+
       <header>
         <h1>HARRY POTTER</h1>
+
         <div className='filter'>
-          <input
-            type="text"
-            placeholder='Nombre de la bruja'
-            id=''
-            name=''
-            // value={}
-            // onInput={}
-          />
+          <form onSubmit={ev => ev.preventDefault()}>
+            <input
+              type="text"
+              placeholder='Nombre de la bruja'
+              id='filterName'
+              name='filterName'
+              value={filters.name}
+              onInput={handleFilterName}
+            />
+          </form>
           <select
-            name=""
-            id=""
-            // value={}
-            // onInput={}
+            name="filterHouse"
+            id="filterHouse"
+            value={filters.house}
+            onInput={handleFilterHouse}
           >
             <option value="">All houses</option>
             <option value="Gryffindor">Gryffindor</option>
@@ -39,10 +88,10 @@ function App() {
             <option value="Hufflepuff">Hufflepuff</option>
           </select>
           <select
-            name=""
-            id=""
-            // value={}
-            // onInput={}
+            name="filterBlood"
+            id="filterBlood"
+            value={filters.ancestry}
+            onInput={handleFilterBlood}
           >
             <option value="">All blood</option>
             <option value="pure-blood">Limpia</option>
@@ -53,7 +102,7 @@ function App() {
         </div>
       </header>
       <main>
-          {magic.map((eachMagic) => (
+          {filteredMagic.map((eachMagic) => (
             <div className='card'>
               <li key={eachMagic.id}>
                 <div className='photo'>
