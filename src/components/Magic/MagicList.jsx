8926321172950e3797419be61473254730fee5f3
raquelpz <rpz.ra.chel@gmail.com>
2025-06-10 18:59:06 +0200

    background houses color

--- src/components/Magic/MagicList.jsx
+++ src/components/Magic/MagicList.jsx
@@ -2,6 +2,14 @@ import MagicItem from "./MagicItem";
 import magicat from '../../images/no_magicat.webp';
 
 function MagicList({ filteredMagic }) {
+
+  const houseClass = {
+    Gryffindor: 'house_red',
+    Slytherin: 'house_green',
+    Ravenclaw: 'house_blue',
+    Hufflepuff: 'house_yellow'
+  };
+
   return (
     <>
       {filteredMagic.length === 0 ? (
@@ -11,7 +19,10 @@ function MagicList({ filteredMagic }) {
         </div>
       ) : (
         filteredMagic.map((eachMagic) => (
-          <div key={eachMagic.id} className='card'>
+          <div
+            key={eachMagic.id}
+            className={`card ${houseClass[eachMagic.house] || ''}`}
+          >
             <MagicItem magic={eachMagic} />
           </div>
         ))
